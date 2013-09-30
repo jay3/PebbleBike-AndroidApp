49e27654b72c1d887a1d381b4438fe4151f0d690
JayPS <dev@jayps.fr>
2013-09-30 21:43:50 +0200

    Save first lat/lon

--- src/com/njackson/GPSService.java
+++ src/com/njackson/GPSService.java
@@ -138,7 +138,16 @@ public class GPSService extends Service {
             _updates = settings.getInt("GPS_UPDATES",0);
         } catch (ClassCastException e) {
             _updates = 0;
-        }        
+        }
+        
+        if (settings.contains("GPS_FIRST_LOCATION_LAT") && settings.contains("GPS_FIRST_LOCATION_LON")) {
+            firstLocation = new Location("PebbleBike");
+            firstLocation.setLatitude(settings.getFloat("GPS_FIRST_LOCATION_LAT", 0.0f));
+            firstLocation.setLongitude(settings.getFloat("GPS_FIRST_LOCATION_LON", 0.0f));
+        } else {
+            firstLocation = null;
+        }
+        
     }
 
     // save the state
@@ -152,6 +161,10 @@ public class GPSService extends Service {
         editor.putLong("GPS_ELAPSEDTIME", _myLocation.getElapsedTime());
         editor.putFloat("GPS_ASCENT", (float) _myLocation.getAscent());
         editor.putInt("GPS_UPDATES", _updates);
+        if (firstLocation != null) {
+            editor.putFloat("GPS_FIRST_LOCATION_LAT", (float) firstLocation.getLatitude());
+            editor.putFloat("GPS_FIRST_LOCATION_LON", (float) firstLocation.getLongitude());
+        }
         editor.commit();
     }
 
@@ -165,6 +178,8 @@ public class GPSService extends Service {
 	    editor.putLong("GPS_ELAPSEDTIME", 0);
 	    editor.putFloat("GPS_ASCENT", 0.0f);
 	    editor.putInt("GPS_UPDATES", 0);
+        editor.putFloat("GPS_FIRST_LOCATION_LAT", 0.0f);
+        editor.putFloat("GPS_FIRST_LOCATION_LON", 0.0f);
 	    editor.commit();
 	    
 	    if (_this != null) {
